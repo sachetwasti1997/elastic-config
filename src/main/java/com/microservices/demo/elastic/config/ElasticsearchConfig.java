1815cbdaa85acc3a789f42ed5165abbab0055fe7
sachetwasti1997 <sachetwasti61@gmail.com>
2024-07-22 18:04:35 +0530

    Added Few dependencies and made elastic search module working again

--- src/main/java/com/microservices/demo/elastic/config/ElasticsearchConfig.java
+++ src/main/java/com/microservices/demo/elastic/config/ElasticsearchConfig.java
@@ -7,14 +7,18 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.config.ElasticsearchConfigurationSupport;
 import org.springframework.data.elasticsearch.core.AbstractElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Objects;
 
 @Configuration
+@EnableElasticsearchRepositories(basePackages = "com.microservices.demo.elastic.index.client.repository")
 public class ElasticsearchConfig {
 
     private final ElasticConfigData elasticConfigData;
